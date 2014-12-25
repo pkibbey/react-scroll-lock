6984c2a241c90675c41b18161e0c6641f1f695ff
Andrey <fxklim@gmail.com>
2014-12-25 13:40:50 +0500

    Add touch cancel

--- src/ScrollLock.js
+++ src/ScrollLock.js
@@ -23,6 +23,36 @@ var removeScrollEventListener = function (elem, handler) {
     elem.removeEventListener('wheel', handler, false);
 };
 
+var addTouchStartEventListener = function(elem, handler) {
+    elem.addEventListener('touchstart', handler, false);
+};
+
+var removeTouchStartEventListener = function(elem, handler) {
+    elem.removeEventListener('touchstart', handler, false);
+};
+
+var addTouchMoveEventListener = function(elem, handler) {
+    elem.addEventListener('touchmove', handler, false);
+};
+
+var removeTouchMoveEventListener = function(elem, handler) {
+    elem.removeEventListener('touchmove', handler, false);
+};
+
+
+var previousTouchEvent = null;
+
+var deltaTouch = function(touchEvent, st) {
+    if (previousTouchEvent) {
+        var delta = -(touchEvent.touches[0].screenY - previousTouchEvent.touches[0].screenY);
+        previousTouchEvent = touchEvent;
+        return delta;
+    }
+    previousTouchEvent = touchEvent;
+    return 0;
+};
+
+
 /**
  * @extends ReactCompositeComponentInterface
  *
@@ -32,14 +62,17 @@ var ScrollLock = {
 
     componentDidMount: function () {
         this.scrollLock();
+        this.touchLock();
     },
 
     componentDidUpdate: function () {
         this.scrollLock();
+        this.touchLock();
     },
 
     componentWillUnmount: function () {
         this.scrollRelease();
+        this.touchRelease();
     },
 
     scrollLock: function () {
@@ -56,6 +89,22 @@ var ScrollLock = {
         }
     },
 
+    touchLock: function () {
+        var elem = this.getDOMNode();
+        if (elem) {
+            addTouchMoveEventListener(elem, this.onTouchHandler);
+            addTouchStartEventListener(elem, this.onTouchStartHandler);
+        }
+    },
+
+    touchRelease: function () {
+        var elem = this.getDOMNode();
+        if (elem) {
+            removeTouchMoveEventListener(elem, this.onTouchHandler);
+            removeTouchStartEventListener(elem, this.onTouchStartHandler);
+        }
+    },
+
     onScrollHandler: function (e) {
         var elem = this.getDOMNode();
         var scrollTop = elem.scrollTop;
@@ -69,6 +118,28 @@ var ScrollLock = {
             return cancelScrollEvent(e);
         }
         else if (!isDeltaPositive && -wheelDelta > scrollTop) {
+            elem.scrollTop = 0;
+            return cancelScrollEvent(e);
+        }
+    },
+
+    onTouchStartHandler: function(e) {
+        deltaTouch(e, this.getDOMNode().scrollTop);
+    },
+
+    onTouchHandler: function(e) {
+        var elem = this.getDOMNode();
+        var scrollTop = elem.scrollTop;
+        var scrollHeight = elem.scrollHeight;
+        var height = elem.clientHeight;
+        var wheelDelta = deltaTouch(e, elem.scrollTop);
+        var isDeltaPositive = wheelDelta > 0;
+
+        if (isDeltaPositive && wheelDelta >= scrollHeight - height - scrollTop) {
+            elem.scrollTop = scrollTop;
+            return cancelScrollEvent(e);
+        }
+        else if (!isDeltaPositive && -wheelDelta >= scrollTop) {
             elem.scrollTop = 0;
             return cancelScrollEvent(e);
         }
